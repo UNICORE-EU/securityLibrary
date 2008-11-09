053c1676d88aebd9448de79d6bffee89d44e3899
Krzysztof Benedyczak <golbi@icm.edu.pl>
2008-11-09 18:47:17 +0000

    indentation in tabs not spaces

--- src/main/java/eu/unicore/security/CertificateUtils.java
+++ src/main/java/eu/unicore/security/CertificateUtils.java
@@ -8,11 +8,8 @@
 
 package eu.unicore.security;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.security.cert.CertificateException;
 import java.security.cert.CertificateExpiredException;
 import java.security.cert.CertificateNotYetValidException;
 import java.security.cert.X509Certificate;
@@ -28,7 +25,7 @@ import eu.unicore.crlcheck.CRLManagerProperties;
  * This class is used internally by other APIs. Every certificate which is used
  * at any <b>validation</b> (e.g. check if ETD assertion is valid) is checked. 
  * By default certificates used in any <b>generation</b> (e.g. creation of Consignor token)
- * is not checked. You can change this behaviour by setting
+ * is not checked. You can change this behavior by setting
  * VERIFY_GENERATION_KEY property to "true".
  * 
  * @author golbi
@@ -36,66 +33,66 @@ import eu.unicore.crlcheck.CRLManagerProperties;
 public class CertificateUtils
 {
 
-  public static final String VERIFY_GENERATION_KEY = "eu.unicore.securty.VerifyExpiredCertUponCreation";
-  public static final String CRLMGR_PROPS_FILE     = "crlmanager.properties.file";
-  
-  private static CRLManager crlManager = null;
+	public static final String VERIFY_GENERATION_KEY = "eu.unicore.securty.VerifyExpiredCertUponCreation";
+	public static final String CRLMGR_PROPS_FILE     = "crlmanager.properties.file";
 
-  static {
-      String crlManagerPropertiesFile=System.getProperty(CRLMGR_PROPS_FILE);
-      if(crlManagerPropertiesFile!=null){
-        CRLManagerProperties crlMgrProps = new CRLManagerProperties();
-        FileInputStream fis=null;
-        try
-        {
-	    fis=new FileInputStream(crlManagerPropertiesFile);
-            crlMgrProps.load(fis);
-        }
-        catch (Exception e)
-        {
-	    System.err.println("Error initialising CRL checker.");
-            e.printStackTrace();
-        }
-        finally{
-	    if(fis!=null)try{fis.close();}catch(IOException ignored){}
-        }
-        crlManager=new CRLManager(crlMgrProps);
-      }
+	private static CRLManager crlManager = null;
 
-  }
+	static {
+		String crlManagerPropertiesFile=System.getProperty(CRLMGR_PROPS_FILE);
+		if(crlManagerPropertiesFile!=null){
+			CRLManagerProperties crlMgrProps = new CRLManagerProperties();
+			FileInputStream fis=null;
+			try
+			{
+				fis=new FileInputStream(crlManagerPropertiesFile);
+				crlMgrProps.load(fis);
+			}
+			catch (Exception e)
+			{
+				System.err.println("Error initialising CRL checker.");
+				e.printStackTrace();
+			}
+			finally{
+				if(fis!=null)try{fis.close();}catch(IOException ignored){}
+			}
+			crlManager=new CRLManager(crlMgrProps);
+		}
 
-  public static void verifyCertificate(X509Certificate cert, boolean doCRLCheck, boolean isGenerateMode) throws CertificateExpiredException, CertificateNotYetValidException
-  {
-    String verify = System.getProperty(CertificateUtils.VERIFY_GENERATION_KEY);
-    if (!isGenerateMode || (verify != null && verify.equals("true"))) cert.checkValidity();
+	}
 
-    if (doCRLCheck && crlManager!=null)
-    {
-      CRLCheckResult cr = crlManager.checkCertificate(cert);
-      if (!cr.isValid())
-      {
-        // TODO Is there a better exception for this?
-        throw new CertificateExpiredException("CRL check failed: "+cr.getReason());
-      }
-    }
-  }
+	public static void verifyCertificate(X509Certificate cert, boolean doCRLCheck, boolean isGenerateMode) throws CertificateExpiredException, CertificateNotYetValidException
+	{
+		String verify = System.getProperty(CertificateUtils.VERIFY_GENERATION_KEY);
+		if (!isGenerateMode || (verify != null && verify.equals("true"))) cert.checkValidity();
 
-  public static void verifyCertificate(X509Certificate[] certs, boolean doCRLCheck, boolean isGenerateMode) throws CertificateExpiredException, CertificateNotYetValidException
-  {
-    for (X509Certificate cert : certs)
-      verifyCertificate(cert, doCRLCheck, isGenerateMode);
-  }
+		if (doCRLCheck && crlManager!=null)
+		{
+			CRLCheckResult cr = crlManager.checkCertificate(cert);
+			if (!cr.isValid())
+			{
+				// TODO Is there a better exception for this?
+				throw new CertificateExpiredException("CRL check failed: "+cr.getReason());
+			}
+		}
+	}
 
-  public static String safePrintSubject(X509Certificate cert)
-  {
-    if (cert == null) return "EMPTY certificate";
-    if (cert.getSubjectX500Principal() == null) return "certificate without a subject";
-    return cert.getSubjectX500Principal().getName();
-  }
+	public static void verifyCertificate(X509Certificate[] certs, boolean doCRLCheck, boolean isGenerateMode) throws CertificateExpiredException, CertificateNotYetValidException
+	{
+		for (X509Certificate cert : certs)
+			verifyCertificate(cert, doCRLCheck, isGenerateMode);
+	}
 
-  public static String safePrintSubject(X509Certificate[] cert)
-  {
-    if (cert == null || cert.length == 0) return "EMPTY certificate";
-    return safePrintSubject(cert[0]);
-  }
+	public static String safePrintSubject(X509Certificate cert)
+	{
+		if (cert == null) return "EMPTY certificate";
+		if (cert.getSubjectX500Principal() == null) return "certificate without a subject";
+		return cert.getSubjectX500Principal().getName();
+	}
+
+	public static String safePrintSubject(X509Certificate[] cert)
+	{
+		if (cert == null || cert.length == 0) return "EMPTY certificate";
+		return safePrintSubject(cert[0]);
+	}
 }
